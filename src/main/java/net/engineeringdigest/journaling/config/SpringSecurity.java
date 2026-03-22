98fedf0f27c605ddf46138832a9f2d9059baf8be
Ashutosh-cm <ashutoshsingh.26362456@gmail.com>
2026-03-22 23:26:55 +0530

    spring security

--- src/main/java/net/engineeringdigest/journaling/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journaling/config/SpringSecurity.java
@@ -4,6 +4,7 @@ import net.engineeringdigest.journaling.services.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,13 +33,16 @@ public class SpringSecurity  {
                    .build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
 
-    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
--- src/main/java/net/engineeringdigest/journaling/services/UserDetailServiceImpl.java
+++ src/main/java/net/engineeringdigest/journaling/services/UserDetailServiceImpl.java
@@ -1,3 +1,6 @@
+
+     //     This class connects our database users with Spring Security’s authentication system
+
 package net.engineeringdigest.journaling.services;
 
 import ch.qos.logback.core.util.ReentryGuard;
@@ -15,18 +18,32 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Autowired
     private UserEntryrepository userEntryrepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { // when ever user try to login this function is called
         user user = userEntryrepository.findByUsername(username);
         if(user!=null){
-            UserDetails userDetails=org.springframework.security.core.userdetails.User.builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .roles(user.getRoles().toArray(new String[0]))
+            UserDetails userDetails=org.springframework.security.core.userdetails.User.builder()  // Then we convert our custom user entity into Spring’s UserDetails object
+                    .username(user.getUsername())   // Sets username
+                    .password(user.getPassword())   // sets password
+                    .roles(user.getRoles().toArray(new String[0]))  // set roles
                     .build();
-            return userDetails;
+            return userDetails;  // spring now compares the entered password v/s stored password and role for authorization
+
 
         }
         throw new UsernameNotFoundException("user not found with username:"+ username);
+        // if the user is not found or password is incorrect then, spring return authorization faliure.
 
     }
+
+    // real flow
+//    1.User enters username/password
+//
+//    2.Spring Security calls:
+//
+//         loadUserByUsername(username)
+//    3.Your code fetches user from DB
+//    4.Returns UserDetails
+//    5.Spring verifies password
+//    6.If correct → login success
+
 }
